fbc1ed9760bfb690ae07185364d10a154afa78f3
chiragcj96 <chiragcj96@gmail.com>
2021-03-30 23:22:16 -0700

    Reverse integer solution updated and algorithm added

--- Reverse Integer/Solution1.py
+++ Reverse Integer/Solution1.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        result = 0
-        multiplier = 10
-        Flag = False
-        if x<0:
+        result = 0                                      #The final result
+        multiplier = 10                                 #to reconstruct the result from individually extracted digits
+        isNegative = False                              #find if inout integer is negative, 
+        if x<0:                                         #if negative, convert it to positive for further operations
             x = -1*x
-            Flag = True
+            isNegative = True                           #Update isNegative Flag to true to reconvert to negative value
         while x !=0:
-            result = (multiplier*result) + (x % 10)
-            # multiplier = multiplier*10
-            x = x / 10
-        if Flag == True:
-            result = -1*result
-        if result > (2**31-1) or result < (-2**31):
+            result = (multiplier*result) + (x % 10)     #construct result by extracting each digit and adding it to the end
+            x = x / 10                                  #update input x each time to remove last digit
+        if isNegative == True:
+            result = -1*result                          #if input was negtive, reconvert result to negative
+        if result > (2**31-1) or result < (-2**31):     #if the final result is beyond the signed 32-bit integer range
             return 0
         return result
