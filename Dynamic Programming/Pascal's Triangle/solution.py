9b0f72f0fd9ff6fb819e385fff1117f58f1d13f4
chiragcj96 <chiragcj96@gmail.com>
2021-05-03 20:02:37 -0700

    Pascal's Triangle

---
+++ Dynamic Programming/Pascal's Triangle/solution.py
@@ -0,0 +1,31 @@
+'''
+Dynamic Programming - 
+matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+
+Using a list[list[]] is used to build a [numRows x numRows] matrix, initialized with all 1's
+We then calculate each value for the pascal triangle by adding -> matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+This populates the whole matrix with correct values
+
+Then we just run for all the levels of the triangles(0->numRows), then for each (x, numRows-x) pairs, we append the values to result
+Return result
+'''
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        matrix = [[1 for _ in range(numRows)] for _ in range(numRows)]
+        result = []
+    
+        for i in range(1, numRows):
+            for j in range(1, numRows):
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+        
+        i = 0
+        while i<numRows:
+            temp = []
+            x = 0
+            while x<=i:
+                temp.append(matrix[x][i-x])
+                x += 1
+            result.append(temp)
+            i += 1
+        return result
+        
