45f60e5ae96d36dc456400bc756657efa09e66f5
chiragcj96 <chiragcj96@gmail.com>
2021-05-30 19:45:47 -0700

    Invert Binary Tree

---  Invert Binary Tree/sol.py
+++  Invert Binary Tree/sol.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Recursive
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if root == None:
@@ -12,4 +14,15 @@ class Solution:
             self.invertTree(root.left)
             self.invertTree(root.right)
             root.left, root.right = root.right, root.left
+        return root
+
+# DFS
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.extend([node.left, node.right])
         return root
